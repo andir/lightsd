463aca1fe591c69242469fdc5d5eaf6b7cc55167
Andreas Rammhold <andreas@rammhold.de>
2017-03-21 17:58:10 +0100

    Added callbacks & type asserts to BoundConcreateValue

--- VariableStore/BoundConcreteValueType.h
+++ VariableStore/BoundConcreteValueType.h
@@ -11,16 +11,25 @@
 
 template<typename EnclosedValue>
 class BoundConcreteValue : public ValueType {
+    using CallbackType = std::function<void (const EnclosedValue&)>;
     std::string name;
     VariableStore &store;
     std::shared_ptr <ConcreteValueType<EnclosedValue >> value;
+    CallbackType callback;
+
+    inline void notify(const EnclosedValue t) {
+        if (callback != nullptr) {
+            callback(value->getValue());
+        }
+    }
 
 public:
-    BoundConcreteValue(const std::string name, const std::string description, VariableStore &store, EnclosedValue initial_value) :
+    BoundConcreteValue(const std::string name, const std::string description, VariableStore &store, EnclosedValue initial_value, CallbackType cb = nullptr) :
             name(name),
             store(store),
             value(std::make_shared<ConcreteValueType<EnclosedValue >>(initial_value)) {
         store.registerVar(name, description, value);
+        callback = cb;
     }
 
     ~BoundConcreteValue() {
@@ -32,19 +41,39 @@ public:
     }
 
     virtual float getFloat() const {
-        return value->getFloat();
+        if (std::is_same<float, EnclosedValue>::value) {
+            return value->getFloat();
+        } else {
+            assert(false && "Invalid getter used");
+        }
     }
 
     virtual int getInteger() const {
-        return value->getInteger();
+        if (std::is_same<int, EnclosedValue>::value) {
+            return value->getInteger();
+        } else {
+            assert(false && "Invalid getter used");
+        }
     }
 
     virtual void setFloat(const float v) {
-        value->setFloat(v);
+        if (std::is_same<float, EnclosedValue>::value) {
+            value->setFloat(v);
+            notify(v);
+        } else {
+            // FIXME: add some fast way of error reporting
+            assert(false && "Invalid setter used");
+        }
     }
 
     virtual void setInteger(const int v) {
-        value->setInteger(v);
+        if (std::is_same<int, EnclosedValue>::value) {
+            value->setInteger(v);
+            notify(v);
+        } else {
+            // FIXME: add some fast way of error reporting
+            assert(false && "Invalid setter used");
+        }
     }
 
     EnclosedValue getValue() const {
