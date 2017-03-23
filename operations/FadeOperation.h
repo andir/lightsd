6cfc28ffe084c2422128cc63429f4329c4590832
Andreas Rammhold <andreas@rammhold.de>
2017-03-23 21:43:27 +0100

    FadeOperaiton: Bail early if the mask is empty

--- operations/FadeOperation.h
+++ operations/FadeOperation.h
@@ -54,6 +54,9 @@ public:
 
     BufferType operator()(BufferType &buffer) {
         std::lock_guard<std::mutex> l(mutex);
+        if (mask.size() == 0)
+            return buffer;
+
         algorithm::PartialMaskBuffer(mask, *buffer, from.getValue());
         return buffer;
     }
