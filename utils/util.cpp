68b7a26b5a499da40a1e447aa76edbd8e9449d42
Andreas Rammhold <andreas@rammhold.de>
2017-03-23 21:46:20 +0100

    utils: removed unused functions

--- utils/util.cpp
+++ utils/util.cpp
@@ -3,15 +3,3 @@
 //
 
 #include "util.h"
-
-
-static int
-getValueByKeyInt(const std::string key, YAML::const_iterator start, YAML::const_iterator end, int default_value) {
-    return getValueByKey<int>(key, start, end, default_value);
-}
-
-static float
-getValueByKeyFloat(const std::string key, YAML::const_iterator start, YAML::const_iterator end, float default_value) {
-    return getValueByKey<float>(key, start, end, default_value);
-}
-
