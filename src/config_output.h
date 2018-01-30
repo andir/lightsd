4a1ba3e4b6e0af074fdaba8af50da9e55aa229e5
Andreas Rammhold <andreas@rammhold.de>
2018-01-30 16:05:46 +0100

    config_output.h: reordered includes

--- src/config_output.h
+++ src/config_output.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include "VariableStore/VariableStore.h"
+#include "outputs/Output.h"
 #include <map>
 #include <memory>
 #include <yaml-cpp/yaml.h>
-#include "outputs/Output.h"
-#include "VariableStore/VariableStore.h"
 
 void parseOutputs(std::map<std::string, std::shared_ptr<Output>>& outputs,
                   const YAML::Node& outputs_node,
