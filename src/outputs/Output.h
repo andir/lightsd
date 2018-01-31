1658182c4da7c9d50fc54ce5d1ec61bf64775886
Andreas Rammhold <andreas@rammhold.de>
2018-01-31 19:39:42 +0100

    Output: support gamma correction

--- src/outputs/Output.h
+++ src/outputs/Output.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include "../Buffer.h"
 #include "../hsv.h"
 
@@ -10,4 +11,9 @@ class Output {
   virtual void draw(const AbstractBaseBuffer<HSV>& buffer) = 0;
 
   virtual void draw(const std::vector<HSV>& buffer) = 0;
+
+  static HSV gammaCorrect(const HSV v, const float factor = 2.2f) {
+    const float newValue = std::pow(v.value, factor);
+    return HSV{v.hue, v.saturation, newValue};
+  }
 };
